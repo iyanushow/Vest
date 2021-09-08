4fc2552c8ab124d5247cc23350a92371f14a89c0
iyanuoluwa <iyanu@kenshomedia.com>
2021-09-08 21:30:23 +0100

    initial config react bootstrapping and added styled components

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <Nav>
+      <Container>
+        <NavLogo>Vest</NavLogo>
+      </Container>
+    </Nav>
+  );
+};
+
+export default Navbar;
---
+++ src/components/Navbar/Navbar.styles.jsx
@@ -0,0 +1,11 @@
+export const Nav = styled.nav`
+  height: 80px;
+  margin-top: -80px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1rem;
+  position: sticky;
+  top: 0;
+  z-index: 10;
+`;
