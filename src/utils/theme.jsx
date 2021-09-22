24bfe1a32e5caa688e45a76f492df3275e81d573
iyanuoluwa <iyanu@kenshomedia.com>
2021-09-23 00:50:28 +0100

    modified theme

--- src/utils/theme.jsx
+++ src/utils/theme.jsx
@@ -1,8 +1,9 @@
-export default {
+const theme = {
   color: {
     defaultBlack: '#010606',
-    // defaultMain: '#01bf71',
     defaultMain: '#2D99B9',
     white: '#fff',
   },
 };
+
+export default theme;
