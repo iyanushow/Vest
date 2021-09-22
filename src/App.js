01018fd885a94a549621fe21312cf9f5d01c79c9
iyanuoluwa <iyanu@kenshomedia.com>
2021-09-23 00:48:32 +0100

    removed navbar from signin page

--- src/App.js
+++ src/App.js
@@ -1,34 +1,25 @@
-import { Fragment, useEffect, useState } from 'react';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
-import Navbar from './components/Navbar';
 import { GlobalStyle } from './utils/globalStyles';
 import theme from './utils/theme';
 import './App.css';
-import Hero from './components/Hero';
-import Info from './components/InfoSection';
+
+import Home from './pages';
+import Auth from './pages/auth';
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const [nav, setNav] = useState([]);
-  useEffect(() => {
-    fetch('/data.json')
-      .then((res) => res.json())
-      .then(({ data, nav }) => {
-        setData(data);
-        setNav(nav);
-      })
-      .catch((err) => console.log(err));
-  }, []);
   return (
     <Router>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
-        <Fragment>
-          <Navbar navItems={nav} />
-          <Hero />
-          <Info data={data} />
-        </Fragment>
+        <Switch>
+          <Route path='/' exact>
+            <Home />
+          </Route>
+          <Route path='/signup' exact>
+            <Auth />
+          </Route>
+        </Switch>
       </ThemeProvider>
     </Router>
   );
