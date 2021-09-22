437eec49887577c995037f14590e87d89110951e
iyanuoluwa <iyanu@kenshomedia.com>
2021-09-23 00:47:46 +0100

    modified cta button for seperate link components

--- src/components/CtaButton/CtaButton.jsx
+++ src/components/CtaButton/CtaButton.jsx
@@ -1,15 +1,35 @@
 import { useState } from 'react';
-import { Button, ForwardArrow, HeroCtaWrapper, RightArrow } from './CtaButton.styles';
+import { Button, ForwardArrow, HeroCtaWrapper, RightArrow, RouterBtn } from './CtaButton.styles';
 
-const CtaButton = ({ route, label, primary, big, dark }) => {
+const CtaButton = ({ route, label, primary, big, dark, type }) => {
   const [hover, setHover] = useState(false);
 
   const toggleHover = () => {
     setHover(!hover);
   };
+
+  if (type === 'router') {
+    console.log(route);
+    return (
+      <HeroCtaWrapper onMouseEnter={toggleHover} onMouseLeave={toggleHover}>
+        <RouterBtn to={route} primary={primary} dark={dark}>
+          {label}
+          {hover ? <ForwardArrow /> : <RightArrow />}
+        </RouterBtn>
+      </HeroCtaWrapper>
+    );
+  }
   return (
     <HeroCtaWrapper onMouseEnter={toggleHover} onMouseLeave={toggleHover}>
-      <Button to={route} primary={primary} dark={dark}>
+      <Button
+        to={route}
+        primary={primary}
+        dark={dark}
+        smooth={true}
+        spy={true}
+        exact='true'
+        offset={-80}
+      >
         {label}
         {hover ? <ForwardArrow /> : <RightArrow />}
       </Button>
--- src/components/CtaButton/CtaButton.styles.jsx
+++ src/components/CtaButton/CtaButton.styles.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import { MdKeyboardArrowRight, MdArrowForward } from 'react-icons/md';
 import { Link } from 'react-scroll';
+import { Link as RLink } from 'react-router-dom';
 
 export const HeroCtaWrapper = styled.div`
   margin-top: 32px;
@@ -31,6 +32,29 @@ export const Button = styled(Link)`
   display: flex;
   justify-content: center;
   transition: all 0.2s ease-in-out;
+  text-decoration: none;
+
+  &:hover {
+    background: ${(props) =>
+      props.primary ? props.theme.color.white : props.theme.color.defaultMain};
+  }
+`;
+
+export const RouterBtn = styled(RLink)`
+  border-radius: 50px;
+  text-transform: capitalize;
+  background: ${(props) =>
+    props.primary ? props.theme.color.defaultMain : props.theme.color.defaultBlack};
+  white-space: nowrap;
+  padding: ${(props) => (props.big ? '14px 48px' : '12px 30px')};
+  color: ${(props) => (props.dark ? props.theme.color.defaultBlack : props.theme.color.white)};
+  font-size: ${(props) => (props.bigFont ? '20px' : '16px')};
+  outline: none;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
 
   &:hover {
     background: ${(props) =>
